24a24bfa30bccdae96baea7d8a9197ec79cdbafd
erikhant <erikd.h49@gmail.com>
2022-10-29 16:23:25 +0700

    added authentication

--- src/api/users/routes.js
+++ src/api/users/routes.js
@@ -6,23 +6,8 @@ const routes = (handler) => [
   },
   {
     method: 'GET',
-    path: '/users',
-    handler: handler.getUsersHandler,
-  },
-  {
-    method: 'GET',
     path: '/users/{id}',
     handler: handler.getUserByIdHandler,
-  },
-  {
-    method: 'PUT',
-    path: '/users/{id}',
-    handler: handler.putUserByIdHandler,
-  },
-  {
-    method: 'DELETE',
-    path: '/users/{id}',
-    handler: handler.deleteUserByIdHandler,
   },
 ];
 
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -26,19 +27,6 @@ class UsersService {
     return result.rows[0].id;
   }
 
-  async verifyNewUsername(username) {
-    const query = {
-      text: 'SELECT username FROM users WHERE username = $1',
-      values: [username],
-    };
-
-    const result = await this._pool.query(query);
-
-    if (result.rows.length > 0) {
-      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
-    }
-  }
-
   async getUserById(userId) {
     const query = {
       text: 'SELECT id, username, fullname FROM users WHERE id = $1',
@@ -52,6 +40,41 @@ class UsersService {
     }
 
     return result.rows[0];
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rows.length > 0) {
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
+    }
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+    return id;
   }
 }
 
